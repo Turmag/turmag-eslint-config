c9b991b2728df3e231115d06d414ecfe860383b8
Pavel Vinogradov <pavel.vinogradov@mpstats.io>
2024-12-22 21:17:25 +0300

    Добавлено правило no-unneeded-ternary

--- index.js
+++ index.js
@@ -77,6 +77,7 @@ export default [
             'object-shorthand': 'error',
             'prefer-const': 'error',
             'arrow-body-style': 'error',
+            'no-unneeded-ternary': 'error',
             '@stylistic/arrow-parens': ['error', 'as-needed'],
             '@stylistic/no-mixed-operators': 'off',
             '@typescript-eslint/no-unused-expressions': 'off',
